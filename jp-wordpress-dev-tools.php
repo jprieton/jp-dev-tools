8ce7ec62f20dd6ad48bb819651687709ab24439e
Javier Prieto <jprieton@gmail.com>
2016-08-22 11:08:14 -0400

    Files/Folder Structure

---
+++ jp-wordpress-dev-tools.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name:    JP WordPress Dev Tools
+ * Version:        0.0.1
+ * Author:         Javier Prieto
+ * License:        GPL-3.0+
+ * License URI:    http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:    jpwpdt
+ * Domain Path:    /languages
+ */
