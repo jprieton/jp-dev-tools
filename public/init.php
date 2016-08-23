363624e090c828d1f2db2ffdde99d3752da8d193
Javier Prieto <jprieton@gmail.com>
2016-08-23 15:44:58 -0400

    Localize public script

--- public/init.php
+++ public/init.php
@@ -14,4 +14,12 @@ add_action( 'wp_enqueue_scripts', function() {
    * @since 0.0.1
    */
   wp_enqueue_script( 'jpwp-public', plugin_dir_url( __FILE__ ) . 'js/public.js', array( 'jquery', 'jquery-form' ), '0.0.1', true );
+
+  /**
+   * Localize public script
+   */
+  $localize_script = array(
+      'ajax_url' => admin_url( 'admin-ajax.php' )
+  );
+  wp_localize_script( 'jpwp-public', 'jpwp', $localize_script );
 } );
