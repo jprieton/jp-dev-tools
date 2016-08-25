b6e71bcb6bba863f649a3f7ad794fc2835b3ac09
Your Name <you@example.com>
2016-08-24 22:48:56 -0430

    HTML & Form classes

--- includes/class-form.php
+++ includes/class-form.php
@@ -19,13 +19,13 @@ if ( !defined( 'ABSPATH' ) ) {
 class Form {
 
   /**
-   * Returns a text input
+   * Retrieve a text input
    *
    * @since 0.0.1
    *
    * @param   string              $name
    * @param   string              $value
-   * @param   string|array        $attributes
+   * @param   string|array        $attributes 
    * @return  string
    */
   public static function text( $name, $value = '', $attributes = array() ) {
@@ -40,62 +40,210 @@ class Form {
   }
 
   /**
-   * Returns a hidden input
-   *
-   * @since 0.0.1
-   *
-   * @param   string              $name
-   * @param   string              $value
-   * @param   string|array        $attributes
-   * @return  string
-   */
-  public static function hidden( $name, $value = '', $attributes = array() ) {
-    $attributes['type'] = 'hidden';
-    return self::text( $name, $value, $attributes );
-  }
-
-  /**
-   * Returns a email input
-   *
-   * @since 0.0.1
-   *
-   * @param   string              $name
-   * @param   string              $value
-   * @param   string|array        $attributes
-   * @return  string
-   */
-  public static function email( $name, $value = '', $attributes = array() ) {
-    $attributes['type'] = 'email';
-    return self::text( $name, $value, $attributes );
-  }
-
-  /**
-   * Returns a password input
-   *
-   * @since 0.0.1
-   *
-   * @param   string              $name
-   * @param   string|array        $attributes
-   * @return  string
-   */
-  public static function password( $name, $attributes = array() ) {
-    $attributes['type'] = 'password';
-    return self::text( $name, '', $attributes );
-  }
-
-  /**
-   * Returns a label element
+   * Retrieve a textarea input
    *
    * @since 0.0.1
    *
    * @param   string              $name
    * @param   string              $text
-   * @param   string|array        $attributes
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function textarea( $name, $text = '', $attributes = array() ) {
+    $defaults   = array(
+        'name' => $name,
+    );
+    $attributes = wp_parse_args( $attributes, $defaults );
+
+    return Html::tag( 'textarea', esc_textarea( $text ), $attributes );
+  }
+
+  /**
+   * Retrieve a hidden input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string              $value
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function hidden( $name, $value = '', $attributes = array() ) {
+    $attributes ['type'] = 'hidden';
+
+    return self::text( $name, $value, $attributes );
+  }
+
+  /**
+   * Retrieve a email input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string              $value
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function email( $name, $value = '', $attributes = array() ) {
+    $attributes ['type'] = 'email';
+
+    return self::text( $name, $value, $attributes );
+  }
+
+  /**
+   * Retrieve a password input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function password( $name, $attributes = array() ) {
+    $attributes ['type'] = 'password';
+
+    return self::text( $name, '', $attributes );
+  }
+
+  /**
+   * Retrieve a file input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function file( $name, $attributes = array() ) {
+    $attributes ['type'] = 'file';
+
+    return self::text( $name, '', $attributes );
+  }
+
+  /**
+   * Retrieve a label element
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string              $text
+   * @param   string|array        $attributes 
    * @return  string
    */
   public static function label( $name, $text, $attributes = array() ) {
-    $attributes['for'] = $name;
+    $attributes ['for'] = $name;
+
     return HTML::_tag( 'label', $text, $attributes );
+  }
+
+  /**
+   * Retrieve a button
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $text
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function button( $text, $attributes = array() ) {
+    $defaults   = array(
+        'type' => 'button'
+    );
+    $attributes = wp_parse_args( $defaults, $attributes );
+
+    return HTML::_tag( 'button', $text, $attributes );
+  }
+
+  /**
+   * Retrieve a submit button
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $text
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function submit( $text = '', $attributes = array() ) {
+    $text       = empty( $text ) ? __( 'Send', 'jpwp' ) : $text;
+    $defaults   = array(
+        'type' => 'submit'
+    );
+    $attributes = wp_parse_args( $attributes, $defaults );
+
+    return self::button( $text, $attributes );
+  }
+
+  /**
+   * Retrieve a checkbox input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string              $value
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function checkbox( $name, $value, $attributes = array() ) {
+    $defaults   = array(
+        'type'    => 'checkbox',
+        'name'    => $name,
+        'value'   => $value,
+        'checked' => is_bool( $attributes ) ? $attributes : false
+    );
+    $attributes = wp_parse_args( $attributes, $defaults );
+
+    if ( $attributes['checked'] ) {
+      $attributes['checked'] = 'checked';
+    } else {
+      unset( $attributes['checked'] );
+    }
+
+    return HTML::_tag( 'input', null, $attributes );
+  }
+
+  /**
+   * Retrieve a radio input
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $name
+   * @param   string              $value
+   * @param   string|array        $attributes 
+   * @return  string
+   */
+  public static function radio( $name, $value, $attributes = array() ) {
+    $defaults   = array(
+        'type'    => 'radio',
+        'name'    => $name,
+        'value'   => $value,
+        'checked' => is_bool( $attributes ) ? $attributes : false
+    );
+    $attributes = wp_parse_args( $attributes, $defaults );
+
+    if ( $attributes['checked'] ) {
+      $attributes['checked'] = 'checked';
+    } else {
+      unset( $attributes['checked'] );
+    }
+
+    return HTML::_tag( 'input', null, $attributes );
+  }
+
+  /**
+   * Retrieve nonce hidden field and action hidden field for forms.
+   * 
+   * @since 0.0.1
+   * 
+   * @param   string              $action
+   * @param   string              $name
+   * @param   bool                $referer
+   * @return  string
+   */
+  public static function nonce( $action, $name = '_wpnonce', $referer = true ) {
+    $field = self::hidden( 'action', $action );
+    $field.= wp_nonce_field( $action, $name, $referer, false );
+
+    return $field;
   }
 
 }
--- includes/class-html.php
+++ includes/class-html.php
@@ -19,15 +19,16 @@ if ( !defined( 'ABSPATH' ) ) {
 class HTML {
 
   /**
-   * Get an HTML img element
+   * Retrieve an HTML img element
    *
    * @since 0.0.1
    *
    * @param   string              $src
-   * @param   string|array        $attributes
+   * @param   string|array        $attributes 
    *
-   * @see http://png-pixel.com/
-   * @return string
+   * @see     http://png-pixel.com/
+   * 
+   * @return  string
    */
   public static function image( $src, $attributes = array() ) {
     if ( 'pixel' == $src ) {
@@ -35,7 +36,63 @@ class HTML {
       $attributes = array_merge( array( 'alt' => 'Pixel' ), (array) $attributes );
     }
     $attributes = wp_parse_args( $attributes, compact( 'src' ) );
+
     return self::_tag( 'img', null, $attributes );
+  }
+
+  /**
+   * Retrieve an HTML script element
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $src
+   * @param   string|array        $attributes 
+   *
+   * @return  string
+   */
+  public static function script( $src, $attributes = array() ) {
+    $attributes = wp_parse_args( $attributes, compact( 'src' ) );
+
+    return self::_tag( 'script', null, $attributes );
+  }
+
+  /**
+   * Retrieve an HTML style element
+   *
+   * @since 0.0.1
+   *
+   * @param   string              $href
+   * @param   string|array        $attributes 
+   *
+   * @return  string
+   */
+  public static function style( $href, $attributes = array() ) {
+    $defaults = array(
+        'href'  => $href,
+        'rel'   => 'stylesheet',
+        'type'  => 'text/css',
+        'media' => 'all',
+    );
+    $attributes = wp_parse_args( $attributes, $defaults );
+
+    return self::_tag( 'link', null, $attributes );
+  }
+
+  /**
+   * Retrieve a HTML link
+   *
+   * @since   0.0.1
+   *
+   * @param   string         $href
+   * @param   string         $text
+   * @param   array          $attributes 
+   * @return  string
+   */
+  public static function link( $href, $text = '', $attributes = array() ) {
+    $text                = is_null( $text ) ? esc_url( $href ) : trim( $text );
+    $attributes ['href'] = $href;
+
+    return self::_tag( 'a', $text, $attributes );
   }
 
   /**
@@ -43,32 +100,33 @@ class HTML {
    *
    * @since   0.0.1
    *
-   * @param   string|array        $attributes
+   * @param   string|array        $attributes 
    * @return  string
    */
   public static function _attributes( $attributes = array() ) {
-    $_attr = array();
+    $_attributes = array();
     foreach ( (array) $attributes as $key => $value ) {
       if ( is_numeric( $key ) ) {
-        $_attr[] = esc_attr( trim( $value ) );
+        $_attributes[] = esc_attr( trim( $value ) );
       } else {
-        $_attr[] = trim( $key ) . '="' . trim( esc_attr( $value ) ) . '"';
+        $_attributes[] = trim( $key ) . '="' . trim( esc_attr( $value ) ) . '"';
       }
     }
-    return implode( ' ', $_attr );
+
+    return implode( ' ', $_attributes );
   }
 
   /**
-   * Returns a HTML tag
+   * Retrieve a HTML tag
    *
    * @since   0.0.1
    *
    * @param   string         $tag
-   * @param   string         $content
-   * @param   array          $attributes
+   * @param   string         $text
+   * @param   array          $attributes 
    * @return  string
    */
-  public static function _tag( $tag, $content = '', $attributes = array() ) {
+  public static function _tag( $tag, $text = '', $attributes = array() ) {
     $self_closing = array( 'area', 'base', 'basefont', 'br', 'hr', 'input', 'img', 'link', 'meta' );
 
     $attributes = self::_attributes( $attributes );
@@ -76,9 +134,10 @@ class HTML {
     if ( in_array( $tag, $self_closing ) ) {
       $html = sprintf( '<%s />', trim( $tag . ' ' . $attributes ) );
     } else {
-      $html = sprintf( '<%s>%s</%s>', trim( $tag . ' ' . $attributes ), $content, $tag );
+      $html = sprintf( '<%s>%s</%s>', trim( $tag . ' ' . $attributes ), $text, $tag );
     }
-    return (string) $html;
+
+    return $html;
   }
 
 }
