dfadae6db405cb61e87d1736cddc96dc5607b84d
Your Name <you@example.com>
2016-08-29 00:05:27 -0430

    Abstract Singleton

---
+++ includes/abstracts/class-singleton.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace JPDevTools\Core;
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
+ * Singleton abstract class
+ *
+ * @package Core\Abstract
+ *
+ * @since   0.0.1
+ *
+ * @author         Javier Prieto <jprieton@gmail.com>
+ */
+abstract class Singleton {
+
+  /**
+   * @since   0.0.1
+   *
+   * @return  static
+   */
+  public static function &get_instance() {
+    if ( !isset( static::$instance ) ) {
+      static::$instance = new static;
+    }
+
+    return static::$instance;
+  }
+
+  /**
+   * @since   0.0.1
+   *
+   * @return  static
+   */
+  protected function __construct() {
+    
+  }
+
+  /**
+   * @since   0.0.1
+   *
+   * @return  static
+   */
+  private function __clone() {
+    
+  }
+
+  /**
+   * @since   0.0.1
+   *
+   * @return  static
+   */
+  private function __wakeup() {
+    
+  }
+
+}
