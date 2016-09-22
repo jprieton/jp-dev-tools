5c2fce2d12b4aba3f04a7436b49cb3bdb1a08fb6
Javier Prieto <jprieton@gmail.com>
2016-09-22 14:30:39 -0400

    Email template

---
+++ templates/email.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <title>{title}</title>
+  </head>
+  <body>
+    <div style="color:#444; max-width: 600px; border: 1px solid #cccccc; padding: 15px; box-shadow: 0 0 2px #999999; margin: auto; font-family:-apple-system,BlinkMacSystemFont,Open-sans,sans-serif;">
+      <h2 style="margin-bottom: 2px; margin-top: 2px;">{title}</h2>
+      <p style="margin-top: 2px; margin-bottom: 2px">{date}</p>
+      <hr style="border: solid 2px #444">
+      <div style="border: solid 1px #cccccc; background-color: #eeeeee; padding: 15px; margin-top: 15px;">{content}</div>
+    </div>
+  </body>
+</html>
